2562ac0e3e974d732adc76b54129d1cb1e80beb1
itzloop <sina.sh.k78@gmail.com>
2023-04-02 03:50:11 +0330

    Fixes undefined global vim in lua files

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,7 +1,6 @@
 local lsp = require('lsp-zero')
 
 lsp.preset("recommended")
-
 lsp.ensure_installed({
 	"gopls",
 	"bashls",
@@ -12,48 +11,46 @@ lsp.ensure_installed({
 	"tsserver",
 	"lua_ls"
 })
-
-
-lsp.on_attach(function(client, bufnr)
-  -- local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  -- local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-  -- Enable completion triggered by <c-x><c-o>
-  -- buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+local on_attach = function(client, bufnr)
+	-- local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+	-- local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+	-- Enable completion triggered by <c-x><c-o>
+	-- buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
 	-- Mappings.
 	local opts = { buffer = bufnr, remap = false }
-	local set = vim.keymap.set
-	local buf = vim.lsp.buf
 
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-	set("n", "gD", function() buf.declaration() end, opts)
-  set("n", 'gd', function() buf.definition() end, opts)
-  set("n", '<leader>K', function() buf.hover() end, opts)
-  set("n", 'gi', function() buf.implementation() end, opts)
-  set("n", '<M-k>', function() buf.signature_help() end, opts)
-  set("n", '<leader>wa', function() buf.add_workspace_folder() end, opts)
-  set("n", '<leader>wr', function() buf.remove_workspace_folder() end, opts)
-	set("n", '<leader>wl', function() print(vim.inspect(buf.list_workspace_folders())) end, opts)
-  set("n", '<leader>D', function() buf.type_definition() end, opts)
-  set("n", '<leader>rn', function() buf.rename() end, opts)
-  set("n", '<leader>ca', function() buf.code_action() end, opts)
-  set("n", 'gr', function() buf.references() end, opts)
-  set("n", '<leader>d', function() vim.lsp.diagnostic.show_line_diagnostics() end, opts)
-  set("n", '[d', function() vim.lsp.diagnostic.goto_prev() end, opts)
-  set("n", ']d', function() vim.lsp.diagnostic.goto_next() end, opts)
-  set("n", '<leader>q', function() vim.lsp.diagnostic.set_loclist() end, opts)
-  set("n", '<C-s>', function() buf.formatting() end, opts)
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
+	vim.keymap.set("n", 'gd', function() vim.lsp.buf.definition() end, opts)
+	vim.keymap.set("n", '<leader>K', function() vim.lsp.buf.hover() end, opts)
+	vim.keymap.set("n", 'gi', function() vim.lsp.buf.implementation() end, opts)
+	vim.keymap.set("n", '<M-k>', function() vim.lsp.buf.signature_help() end, opts)
+	vim.keymap.set("n", '<leader>wa', function() vim.lsp.buf.add_workspace_folder() end, opts)
+	vim.keymap.set("n", '<leader>wr', function() vim.lsp.buf.remove_workspace_folder() end, opts)
+	vim.keymap.set("n", '<leader>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, opts)
+	vim.keymap.set("n", '<leader>D', function() vim.lsp.buf.type_definition() end, opts)
+	vim.keymap.set("n", '<leader>rn', function() vim.lsp.buf.rename() end, opts)
+	vim.keymap.set("n", '<leader>ca', function() vim.lsp.buf.code_action() end, opts)
+	vim.keymap.set("n", 'gr', function() vim.lsp.buf.references() end, opts)
+	vim.keymap.set("n", '<leader>d', function() vim.lsp.diagnostic.show_line_diagnostics() end, opts)
+	vim.keymap.set("n", '[d', function() vim.lsp.diagnostic.goto_prev() end, opts)
+	vim.keymap.set("n", ']d', function() vim.lsp.diagnostic.goto_next() end, opts)
+	vim.keymap.set("n", '<leader>q', function() vim.lsp.diagnostic.set_loclist() end, opts)
+	vim.keymap.set("n", '<C-s>', function() vim.lsp.buf.format({ async = true }) end, opts)
 
-  if client.server_capabilities.document_formatting then
-    vim.api.nvim_exec([[
+	if client.server_capabilities.document_formatting then
+		vim.api.nvim_exec([[
      augroup LspAutocommands
          autocmd! * <buffer>
          autocmd BufWritePost <buffer> lua buf.formatting()
      augroup END
      ]], true)
-  end
-end)
+	end
+end
+lsp.on_attach(on_attach)
 -- (Optional) Configure lua language server for neovim
 -- require('lspconfig').lua_ls.setup(require('lsp-zero').nvim_lua_ls())
-
 lsp.setup()
+
+require("lsp.lua-ls-config")(on_attach)
--- lua/lsp/lua-ls-config.lua
+++ lua/lsp/lua-ls-config.lua
@@ -1,56 +1,14 @@
-local nvim_lsp = require('lspconfig')
-
-local system_name
-if vim.fn.has("mac") == 1 then
-  system_name = "macOS"
-elseif vim.fn.has("unix") == 1 then
-  system_name = "Linux"
-elseif vim.fn.has('win32') == 1 then
-  system_name = "Windows"
-else
-  print("Unsupported system for sumneko")
-end
-
--- set the path to the sumneko installation; if you previously installed via the now deprecated :LspInstall, use
-local sumneko_root_path = vim.fn.stdpath('config')..'/language-servers/lua-language-server'
--- local sumneko_binary = sumneko_root_path.."/bin/"..system_name.."/lua-language-server"
-local sumneko_binary = sumneko_root_path.."/bin/lua-language-server"
-
-local runtime_path = vim.split(package.path, ';')
-table.insert(runtime_path, "lua/?.lua")
-table.insert(runtime_path, "lua/?/init.lua")
 local function setup(on_attach)
-  -- print("setting up lua ls")
-  nvim_lsp.sumneko_lua.setup {
-      capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities()),
-    cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
-    settings = {
-      Lua = {
-        runtime = {
-          -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-          version = 'LuaJIT',
-          -- Setup your lua path
-          path = runtime_path,
-        },
-        diagnostics = {
-          -- Get the language server to recognize the `vim` global
-          globals = {'vim'},
-        },
-        workspace = {
-          -- Make the server aware of Neovim runtime files
-          library = vim.api.nvim_get_runtime_file("", true),
-        },
-        -- Do not send telemetry data containing a randomized but unique identifier
-        telemetry = {
-          enable = false,
-        },
-      },
-    },
-    on_attach = on_attach,
-    flags = {
-      debounce_text_changes = 150,
-    },
-  }
+	require'lspconfig'.lua_ls.setup {
+		settings = {
+			Lua = {
+				diagnostics = {
+					-- Get the language server to recognize the `vim` global
+					globals = {'vim'},
+				},
+			},
+		},
+	}
 end
 
 return setup
